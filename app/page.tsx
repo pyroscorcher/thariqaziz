dd17d6dfee4b3542d4f47ef773eaccb6e81fd3f6
pyroscorcher <pyroscorcherthariq@apps.ipb.ac.id>
2026-07-10 16:02:32 +0700

    lanjut nanti

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <main className="overflow-hidden">
+    <main className="overflow-hidden isolate bg-white">
 
       <div>
         <img src="resources/phonify1.png" alt="" className="absolute inset-0 -z-10 h-full w-full object-cover object-top" />
@@ -96,15 +96,35 @@ export default function Home() {
             </div>
           </div>
         </div>
+
       </div>
 
-      <div>
-        <div className="z-10 space-x-6">
-          <button className="relative overflow-hidden rounded-full border-2 border-gray-800 bg-transparent py-6 px-[3.75rem] font-medium uppercase text-gray-800 transition-colors before:absolute before:inset-0 before:-z-10 before:origin-bottom before:scale-y-0 before:bg-gray-800 before:transition-transform before:duration-300 before:content-[''] hover:text-white hover:before:origin-top hover:before:scale-y-100">
-            Hover Me
+      <div className="w-full px-8 py-24 md:px-16 lg:px-32 xl:px-48">
+        <div className="flex flex-col gap-12 lg:flex-row lg:items-center lg:gap-24">
+          
+          <div className="lg:w-3/5">
+            <h2 className="font-popins text-3xl font-medium leading-[1.3] text-gray-900 md:text-4xl lg:text-[2.75rem]">
+              Bringing a proactive approach to learning, collaboration, and innovation across technical domains.
+            </h2>
+          </div>
+
+          <div className="lg:w-2/5 lg:pl-10">
+            <p className="font-popins text-sm leading-relaxed text-gray-700 md:text-base">
+              Ambitious and driven undergraduate student, with strong communication skill. Skilled in application and system development as well as UI/UX design.
+            </p>
+          </div>
+
+        </div>
+
+        <div className="flex items-center justify-center h-screen">
+          <button className="relative overflow-hidden rounded-full border-2 border-gray-800 bg-transparent py-6 px-[3.75rem] font-medium text-gray-800 transition-colors before:absolute before:inset-0 before:-z-10 before:origin-bottom before:scale-y-0 before:bg-gray-800 before:transition-transform before:duration-300 before:content-[''] hover:text-white hover:before:origin-top hover:before:scale-y-100">
+            More work
           </button>
         </div>
+
       </div>
+
+
 
     </main>
   );
