f9f7b65a06bec43174402b61b39af4dcebe3cbc2
pyroscorcher <pyroscorcherthariq@apps.ipb.ac.id>
2026-09-11 15:00:11 +0700

    navbar separation

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,7 @@
-'use client';
-
 import "tailwindcss";
-import { useState } from "react";
-
-const navigation = [
-  { name: 'Work', href: '#work' },
-  { name: 'Contact', href: '#contact' },
-]
-
-import { Dialog, DialogPanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Navbar from "./components/navbar";
 
 export default function Home() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
   return (
     <main className="overflow-hidden isolate bg-white">
 
@@ -21,68 +9,7 @@ export default function Home() {
         <img src="resources/phonify1.png" alt="" className="absolute inset-0 -z-10 h-full w-full object-cover object-top" />
         <div className="absolute inset-0 -z-10"/>
 
-        <header className="absolute inset-x-0 top-0 z-50">
-          <nav aria-label="Global" className="flex items-center justify-center p-12 lg:px-8">
-            <div className="flex lg:hidden w-full justify-end">
-              {/* I added w-full and justify-end here so your mobile hamburger menu stays on the right side on small screens! */}
-              <button
-                type="button"
-                onClick={() => setMobileMenuOpen(true)}
-                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-200"
-              >
-                <span className="sr-only">Open main menu</span>
-                <Bars3Icon aria-hidden="true" className="size-6" />
-              </button>
-            </div>
-            
-            <div className="hidden lg:flex lg:gap-x-12">
-              {navigation.map((item) => (
-                <a key={item.name} href={item.href} className="text-[38px] leading-6 font-bold font-popins text-white">
-                  {item.name}
-                </a>
-              ))}
-            </div>
-          </nav>
-          
-          <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-            <div className="fixed inset-0 z-50" />
-            <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
-              <div className="flex items-center justify-between">
-                <a href="#" className="-m-1.5 p-1.5">
-                  <span className="sr-only">Your Company</span>
-                  <img
-                    alt=""
-                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                    className="h-8 w-auto"
-                  />
-                </a>
-                <button
-                  type="button"
-                  onClick={() => setMobileMenuOpen(false)}
-                  className="-m-2.5 rounded-md p-2.5 text-gray-200"
-                >
-                  <span className="sr-only">Close menu</span>
-                  <XMarkIcon aria-hidden="true" className="size-6" />
-                </button>
-              </div>
-              <div className="mt-6 flow-root">
-                <div className="-my-6 divide-y divide-white/10">
-                  <div className="space-y-2 py-6">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className="-mx-3 block rounded-lg px-3 py-2 text-xl/6 font-semibold text-white hover:bg-white/5"
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </DialogPanel>
-          </Dialog>
-        </header>
+        <Navbar />
 
         <div className="relative isolate px-6 pt-48 lg:px-8">
           <div className="overflow-hidden py-32 sm:py-48 lg:py-56 flex w-full">
